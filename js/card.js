babc718e367a57cbef7948081d827d353138a6d4
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-15 11:43:31 -0300

    Update card.js

--- js/card.js
+++ js/card.js
@@ -1,50 +1,67 @@
 function criarCard(item, tipo = "movie") {
 
-    const titulo = item.title || item.name || "Sem título";
+    const titulo = item.title || item.name;
 
     const poster = item.poster_path
-        ? `${CONFIG.IMAGE_POSTER}${item.poster_path}`
-        : "https://via.placeholder.com/342x513?text=Sem+Imagem";
+        ? CONFIG.IMAGE_POSTER + item.poster_path
+        : "https://via.placeholder.com/342x513";
 
     const nota = item.vote_average
         ? item.vote_average.toFixed(1)
-        : "0.0";
+        : "-";
+
+    const ano = (
+        item.release_date ||
+        item.first_air_date ||
+        ""
+    ).substring(0,4);
 
     return `
-        <div class="card"
-             data-id="${item.id}"
-             data-tipo="${tipo}">
+
+        <div
+            class="card"
+            data-id="${item.id}"
+            data-tipo="${tipo}">
 
             <img
                 src="${poster}"
-                alt="${titulo}"
-                loading="lazy">
+                loading="lazy"
+                alt="${titulo}">
 
-            <div class="card-info">
+            <div class="card-overlay">
 
-                <h3>${titulo}</h3>
+                <div class="card-title">
 
-                <span class="nota">
-                    ⭐ ${nota}
-                </span>
+                    ${titulo}
+
+                </div>
+
+                <div class="card-meta">
+
+                    <span class="card-rating">
+
+                        ⭐ ${nota}
+
+                    </span>
+
+                    <span>
+
+                        ${ano}
+
+                    </span>
+
+                </div>
+
+                <button class="card-btn">
+
+                    Ver detalhes
+
+                </button>
 
             </div>
 
         </div>
+
     `;
-}
-function renderizarCards(containerId, lista, tipo = "movie") {
-
-    const container = document.getElementById(containerId);
-
-    if (!container) return;
-
-    lista.forEach(item => {
-
-        if (!item.poster_path) return;
-
-        container.innerHTML += criarCard(item, tipo);
-
-    });
 
 }
