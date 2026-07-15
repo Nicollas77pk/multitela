257a0cdf8e4dae5504199ff2da011446d80efdcd
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-15 10:46:31 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,19 @@
+const CONFIG = {
 
+    SITE_NAME: "Multitela",
+
+    API_KEY: "841164866e07eba1e3ccb100a9d370fb",
+
+    API_URL: "https://api.themoviedb.org/3",
+
+    IMAGE_URL: "https://image.tmdb.org/t/p",
+
+    IMAGE_POSTER: "https://image.tmdb.org/t/p/w342",
+
+    IMAGE_BACKDROP: "https://image.tmdb.org/t/p/w780",
+
+    LANGUAGE: "pt-BR",
+
+    COUNTRY: "BR"
+
+};
