f93ad0c7ca638b43436158a4b8f1f7dc7ece956d
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-17 10:58:18 -0300

    Create providers.js

---
+++ js/providers.js
@@ -0,0 +1,23 @@
+const PROVIDERS = {
+
+    "Netflix": "https://www.netflix.com/br/",
+
+    "Prime Video": "https://www.primevideo.com/",
+
+    "Disney Plus": "https://www.disneyplus.com/pt-br",
+
+    "Max": "https://play.max.com/",
+
+    "Globoplay": "https://globoplay.globo.com/",
+
+    "Apple TV Plus": "https://tv.apple.com/br",
+
+    "Crunchyroll": "https://www.crunchyroll.com/pt-br",
+
+    "Paramount Plus": "https://www.paramountplus.com/br/",
+
+    "Looke": "https://www.looke.com.br/",
+
+    "MUBI": "https://mubi.com/pt/br"
+
+};
